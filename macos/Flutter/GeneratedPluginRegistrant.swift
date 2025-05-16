bc56eb8581bbc2f20635fed52d86747dc304bfc3
Benjamin Laughton <benjamin@Benjamins-MacBook-Air.local>
2025-05-16 13:34:23 +0200

    Add full Flutter project structure

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cryptography_flutter
+import device_info_plus
+import shared_preferences_foundation
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CryptographyFlutterPlugin.register(with: registry.registrar(forPlugin: "CryptographyFlutterPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
